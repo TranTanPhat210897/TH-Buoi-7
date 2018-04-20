fa8f4acdb6eb266d70a20cf249de7db0585209d7
TranTanPhat210897 <15DH110265TTP@gmail.com>
2018-04-20 19:50:36 +0700

    TH-Buoi-7

---
+++ Broadcast/Broadcast/SB.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Drawing;
+using System.Net.NetworkInformation;
+using System.Timers;
+using System.Threading;
+
+namespace Broadcast
+{
+    class SB
+    {
+        static void Main(string[] args)
+        {
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint iep1 = new IPEndPoint(IPAddress.Broadcast, 9050);
+            IPEndPoint iep2 = new IPEndPoint(IPAddress.Parse("192.168.1.254"), 9050);
+            string hostname = Dns.GetHostName();
+            int prevTick = 0;
+            while (true)
+            {
+                    byte[] data = Encoding.ASCII.GetBytes(hostname);
+                    sock.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+                    sock.SendTo(data, iep1);
+                    sock.SendTo(data, iep2);
+                    sock.Close();
+                    Thread.Sleep(1000);
+            }
+        }
+    }
+}
